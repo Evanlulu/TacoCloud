108cf1a7b1f48618c45b8470de09407982f95f6d
EvanLu <coffeelululu@gmail.com>
2023-09-20 07:48:27 +0800

    taco-cloud web controller,entity

---
+++ src/test/java/evan/tacocloud/tacos/Taco.java
@@ -0,0 +1,13 @@
+package evan.tacocloud.tacos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
