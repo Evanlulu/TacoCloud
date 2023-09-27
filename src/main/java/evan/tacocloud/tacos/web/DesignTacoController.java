553939a157323db0953145d359cb8735b4d4ed0f
EvanLu <coffeelululu@gmail.com>
2023-09-28 07:52:18 +0800

    taco-cloud order process Converter

--- src/main/java/evan/tacocloud/tacos/web/DesignTacoController.java
+++ src/main/java/evan/tacocloud/tacos/web/DesignTacoController.java
@@ -6,10 +6,7 @@ import evan.tacocloud.tacos.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,11 +28,11 @@ public class DesignTacoController {
                 new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
                 new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
                 new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.WRAP),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
                 new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey", Ingredient.Type.WRAP),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.WRAP),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.WRAP)
+                new Ingredient("JACK", "Monterrey", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
         );
 
         Ingredient.Type[] types = Ingredient.Type.values();
@@ -59,10 +56,20 @@ public class DesignTacoController {
         return "design";
     }
 
+    @PostMapping
+    public String processTaco(Taco taco,
+                              @ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco : {}",taco);
+
+        return "redirect:/orders/current";
+    }
+
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients,Ingredient.Type type){
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
+
 }
